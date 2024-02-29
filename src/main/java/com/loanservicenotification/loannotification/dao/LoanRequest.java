dae1204ec38aecd2be293eae6bc6a18cc6f0cc87
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2024-02-29 14:45:06 +0300

    LoanRequest dao done

--- src/main/java/com/loanservicenotification/loannotification/dao/LoanRequest.java
+++ src/main/java/com/loanservicenotification/loannotification/dao/LoanRequest.java
@@ -1,4 +1,47 @@
 package com.loanservicenotification.loannotification.dao;
 
 public class LoanRequest {
+
+    private Integer Id;
+    private String borrower;
+
+    private Integer amount;
+
+    public LoanRequest(Integer id, String borrower, Integer amount) {
+        Id = id;
+        this.borrower = borrower;
+        this.amount = amount;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanRequest{" +
+                "borrower='" + borrower + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
