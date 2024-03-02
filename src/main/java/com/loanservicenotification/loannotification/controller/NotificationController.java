871ba4a4a59e218e937d4a35b4077dc00c1dc061
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2024-03-02 15:36:27 +0300

    Code refactored

--- src/main/java/com/loanservicenotification/loannotification/controller/NotificationController.java
+++ src/main/java/com/loanservicenotification/loannotification/controller/NotificationController.java
@@ -1,4 +1,24 @@
 package com.loanservicenotification.loannotification.controller;
 
+import com.loanservicenotification.loannotification.dao.LoanRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
 public class NotificationController {
+
+//    @Autowired
+//    private  final LoanRequest loanRequest;
+
+//    public NotificationController(LoanRequest loanRequest) {
+//        this.loanRequest = loanRequest;
+//    }
+
+    @PostMapping("/notify")
+    public  void notifyUser(@RequestBody LoanRequest loanRequest){
+        System.out.println("Hello " + " " + loanRequest.getBorrower()+ " " + "Your loan amount of" + " " + " " +  loanRequest.getAmount() + "has been processed Successfully" );
+
+    }
+
 }
--- src/main/java/com/loanservicenotification/loannotification/dao/LoanRequest.java
+++ src/main/java/com/loanservicenotification/loannotification/dao/LoanRequest.java
@@ -1,5 +1,8 @@
 package com.loanservicenotification.loannotification.dao;
 
+//import org.springframework.stereotype.Component;
+//
+//@Component
 public class LoanRequest {
 
     private Integer Id;
